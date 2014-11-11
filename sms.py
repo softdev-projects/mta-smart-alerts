e2b9c11a58b0ff1108b047b68c97885477702544
Genji Noguchi <genji.noguchi@gmail.com>
2014-11-11 15:45:23 -0500

    forgot something oops

--- sms.py
+++ sms.py
@@ -4,8 +4,8 @@ account_sid = "PNc7a6199f75736a4291760f00695b233c"
 auth_token  = ""
 client = TwilioRestClient(account_sid, auth_token)
  
-def send_message(message="hardcoded"):
+def send_message(message="hardcoded",number=9174350162):
     message = client.messages.create(body=message,
-    to="+9174350162",
+    to="+" + str(number),
     from_="+4694782929")
     print message.sid
