1d41595244af051882faa56c0d3325d5c2f15c97
Justin Kim <yulli@yulli.org>
2014-11-11 18:35:56 -0500

    Render the status page for the homepage

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    # return render_template("home.html")
+    return statusPage()
 
 
 @app.route("/login", methods=["GET", "POST"])
